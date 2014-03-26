03c48caf35d89daee157e1b7394d085d17bf090a
matt <matt.jones@monster.com>
2014-03-26 13:35:02 -0400

    Fix all known issues with CEng_add_service_check.py
    Finished working on the script, and tested it in a live environment nd saw no issues.  It still needs work as it is just a basic script right now, but can easily be modified to include more object definitions it the user wants.

--- ManagementScripts/CEng_add_service_check.py
+++ ManagementScripts/CEng_add_service_check.py
@@ -20,49 +20,93 @@
 
 from string import Template
 import sys
+import argparse
 
 def main():
 
-  # Need to use argparse to set these properly
-  InheritanceTemplate = sys.argv[1]
-  MonitoredInstance = sys.argv[2]
-  ServiceDescription = sys.argv[3]
-  ServiceCommand = sys.argv[4]
-  CommandArguement1 = sys.argv[5]
-  CommandArguement2 = sys.argv[6]
-  HostConfigList =  = sys.argv[7]
+  parser = argparse.ArgumentParser(description='Add new service check')
+  parser.add_argument('--InheritanceTemplate', help='a seperate service definition template you wish to inherit from', required=False)
+  parser.add_argument('--MonitoredInstance', help='the hostname of the server you wish to monitor', required=True)
+  parser.add_argument('--ServiceDescription', help='this will define the description of the service', required=True)
+  parser.add_argument('--ServiceCommand', help='the command used to run the service, must be defined in a command file first', required=True)
+  parser.add_argument('--CommandArguement1', help='the first commandline arguement', required=False)
+  parser.add_argument('--CommandArguement2', help='the second commandline arguement', required=False)
+  parser.add_argument('--CommandArguement3', help='the third commandline arguement', required=False)
+  parser.add_argument('--HostConfigList', help='a list of hosts you wish to add the check to', required=True)
+  args = vars(parser.parse_args())
+
+  if args['InheritanceTemplate']:
+    InheritanceTemplate = args['InheritanceTemplate']
+  else:
+    InheritanceTemplate = ''
+  
+  if args['MonitoredInstance']:
+    MonitoredInstance = args['MonitoredInstance']
+  
+  if args['ServiceDescription']:
+    ServiceDescription = args['ServiceDescription']
+  
+  if args['ServiceCommand']:
+    ServiceCommand = args['ServiceCommand']
+    ServiceCommandLine = "$service_command"
+  
+  if args['CommandArguement1']:
+    CommandArguement1 = args['CommandArguement1']
+    ServiceCommand = ServiceCommand + "!" + CommandArguement1
+  
+  if args['CommandArguement2']:
+    CommandArguement2 = args['CommandArguement2']
+    ServiceCommand = ServiceCommand + "!" + CommandArguement2
+  
+  if args['CommandArguement3']:
+    CommandArguement3 = args['CommandArguement3']
+    ServiceCommand = ServiceCommand + "!" + CommandArguement3
+  
+  if args['HostConfigList']:
+    HostConfigList = args['HostConfigList']
+  
 
   # a very basic service definition, a better one with all available optitions needs to be created
   # optitions should not be included in the template if the variable is not defined which means the template will need to be
   # created dynamically based upon what argparse says is present
-  ServiceTemplate = Template('''
+  
+  if InheritanceTemplate:
+    ServiceTemplate = Template('''
 define service{
           use                             $inheritance_template
           host_name                       $monitored_instance
           service_description             $service_description
-          check_command                   $service_command!$service_command_arg1!$service_command_arg2
+          check_command                   $service_command
+          }
+  ''')
+  else:
+    ServiceTemplate = Template('''
+define service{
+          host_name                       $monitored_instance
+          service_description             $service_description
+          check_command                   $service_command
           }
   ''')
 
+
   # I don't want to use safe substitutions as I want it to error rather than add a bad service check to the configuration file
-  ServiceCheck = ServiceTemplate.substitute({ 'inheritance_template': "InheritanceTemplate", 'monitored_instance': "MonitoredInstance", 'service_description': "ServiceDescription", 'service_command': "ServiceCommand", 'service_command_arg1': "CommandArguement1", 'service_command_arg2': "CommandArguement2"})
-
+  ServiceCheck = ServiceTemplate.substitute({ 'inheritance_template': InheritanceTemplate, 'monitored_instance': MonitoredInstance, 'service_description': ServiceDescription, 'service_command': ServiceCommand})
+  
   # open a file containing a lists of hosts you want to add the check to and read it into a list
-  with open(HostConfigList) as f:
-     HostList= f.readlines()
-
+  with open(HostConfigList) as hl:
+     HostList= hl.readlines()
+  
   # iterate over the list of hosts
   # still need to find a way to locate the correct file in the filesystem, something along the lines of 'locate -b'
   # currently this will need to be run in the same directory as the configuration files
   i = iter(HostList)
-  ConfigFile = i.next() + '.cfg'
-
+  ConfigFile = i.next().rstrip() + '.cfg'
+  
   # append the new check to the end of the file
-  f = open( "ConfigFile", 'a' )
+  f = open( ConfigFile, 'a' )
   f.write(ServiceCheck)
   f.close()
-
-  #print ServiceCheck
+  hl.close()
 
 if __name__ == "__main__":
     main()
