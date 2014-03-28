52560aed24ce09ecdaaa855a256707d681668633
matt <matt.jones@monster.com>
2014-03-28 17:38:07 -0400

    Convert script to use argparse
    For details on why this was done see README features section.

--- ServiceChecks/CEng_check_vol_rw_2.py
+++ ServiceChecks/CEng_check_vol_rw_2.py
@@ -12,11 +12,11 @@ CEng_check_vol_rw_2
           permissions.
  Usage:
 
- Command Line 1:  ./CEng_check_rw_2.py
+ Command Line 1:  ./CEng_check_rw_2.py --warning <yes/no> --unknown <yes/no> --critical <yes/no>
 
  NRPE Examples   ./check_nrpe -H hal2k1.foo.example.com -c CEng_check_rw_2.py
 
- Local Example:  /CEng_check_rw_2.py
+ Local Example:  /CEng_check_rw_2.py --critical no
 
 '''
 
@@ -24,63 +24,101 @@ from datetime import datetime
 import sys
 import subprocess
 import os
-
+import argparse
 
 def main():
-    # Execution start time
-    start_time = datetime.now()
 
-    # Get the uid of the user executing the script
-    uid = subprocess.Popen(['id', '-u'], stdout=subprocess.PIPE)
-    uid = uid.stdout.read()
-    uid = uid.rstrip()
+  # enable default alerting
+  OK = ceng_lib.OK
+  WARNING = ceng_lib.WARNING
+  CRITICAL = ceng_lib.CRITICAL
+  UNKNOWN = ceng_lib.UNKNOWN
 
-    # This script needs to run as root
-    if uid != '0':
-        print 'You must be root to run this'
-        sys.exit(3)
+  parser = argparse.ArgumentParser(description='This script will get a list of mount points from /proc/self/mounts to see if they are read only.')
+  parser.add_argument('--warning', choices=['yes', 'no'], default ='yes', help='enable warning alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--critical', choices=['yes', 'no'], default ='yes', help='enable critical alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--unknown', choices=['yes', 'no'], default ='yes', help='enable unknown alerts and status\'s for this check (default: yes)')
+  args = vars(parser.parse_args())
+
+  # check for a ctitical state, if so and warning is not set to no, then set critical to warning
+  if args['critical']:
+    if args['critical'] == 'no' and args['warning'] == 'no':
+      CRITICAL = OK
+      elif args['critical'] == 'no' and args['warning'] != 'no':
+        CRITICAL = WARNING
     else:
-        pass
+      CRITICAL = CRITICAL
 
-    exit_code = 0
+  # check for a warning state, if so and critical is not set to no, then set warning to critical
+  if args['warning']:
+    if args['warning'] == 'no' and args['critical'] == 'no':
+      WARNING = OK
+    elif args['warning'] == 'no' and args['critical'] != 'no':
+      WARNING = OK
+    else:
+      WARNING = WARNING
 
-    # The directory to write into
-    test_dir = '/lost+found'
+  # if unknown is set to no, then set unknown to ok
+  if args['unknown']:
+    if args['unknown'] == 'no':
+      UNKNOWN = OK
+      else:
+        UNKNOWN = UNKNOWN
 
-    # The mount points to check
-    mount_points = ['', '/boot', '/home', '/usr', '/var', '/tmp', '/var/log']
+  # Execution start time
+  start_time = datetime.now()
 
-    # List to hold any read-only directories
-    ro_dir = []
+  # Get the uid of the user executing the script
+  uid = subprocess.Popen(['id', '-u'], stdout=subprocess.PIPE)
+  uid = uid.stdout.read()
+  uid = uid.rstrip()
 
-    i = iter(mount_points)
-    for d in mount_points:
-        working_directory = i.next() + test_dir
-        if not os.path.isdir(working_directory):
-            subprocess.Popen(['mkdir', '-m', '0700', working_directory])
-        else:
-            subprocess.Popen(['chown', '0:0', working_directory])
-            subprocess.Popen(['mkdir', 'working_directory', '/.ro'])
-            output = subprocess.Popen(['rmdir', 'working_directory', '/.ro'], stdout=subprocess.PIPE)
-            output = output.stdout.read()
+  # This script needs to run as root
+  if uid != '0':
+    print 'You must be root to run this'
+    sys.exit(CRITICAL)
+  else:
+    pass
 
-            if output:
-                ro_dir.append(working_directory)
-                exit_code = 2
-            else:
-                if exit_code != 2:
-                    exit_code = 0
+    ExitCode = OK
 
-    # The script run time
-    run_time = datetime.now() - start_time
+  # The directory to write into
+  test_dir = '/lost+found'
 
-    if exit_code == 2:
-        print (ro_dir, ' are Read-Only | \'Check_Time\'=%s;;;0.000000;60.000000;' % (run_time))
-        sys.exit(exit_code)
+  # The mount points to check
+  mount_points = ['', '/boot', '/home', '/usr', '/var', '/tmp', '/var/log']
 
-    elif exit_code == 0:
-        print ('All directories are Read/Write | \'Check_Time\'=%s;;;0.000000;60.000000;' % (run_time))
-        sys.exit(exit_code)
+  # List to hold any read-only directories
+  ro_dir = []
+
+  i = iter(mount_points)
+  for d in mount_points:
+    working_directory = i.next() + test_dir
+    if not os.path.isdir(working_directory):
+      subprocess.Popen(['mkdir', '-m', '0700', working_directory])
+    else:
+      subprocess.Popen(['chown', '0:0', working_directory])
+      subprocess.Popen(['mkdir', 'working_directory', '/.ro'])
+      output = subprocess.Popen(['rmdir', 'working_directory', '/.ro'], stdout=subprocess.PIPE)
+      output = output.stdout.read()
+
+  if output:
+    ro_dir.append(working_directory)
+    exit_code = WARNING
+  else:
+    if exit_code != WARNING:
+      exit_code = OK
+
+  # The script run time
+  run_time = datetime.now() - start_time
+
+  if ExitCode == CRITICAL:
+    print (ro_dir, ' are Read-Only | \'Check_Time\'=%s;;;0.000000;60.000000;' % (run_time))
+    sys.exit(ExitCode)
+
+  elif ExitCode == OK:
+    print ('All directories are Read/Write | \'Check_Time\'=%s;;;0.000000;60.000000;' % (run_time))
+    sys.exit(ExitCode)
 
 
 if __name__ == "__main__":
