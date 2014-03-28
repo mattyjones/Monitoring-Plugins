1bb7d324018da7537fa4f39cf2bd0109b124ed65
matt <matt.jones@monster.com>
2014-03-28 11:13:46 -0400

    Convert script to use argparse
    Convert the script to use argparse instead of sys.argv.  The script now has 2 positional arguments (warning and critical thresholds) and 4 opitional arguments (warning, critical, unknown, help)

--- ServiceChecks/CEng_check_physical_mem.py
+++ ServiceChecks/CEng_check_physical_mem.py
@@ -14,11 +14,11 @@
 
  Usage:
 
- Command Line 1:  ./icinga_check_memory.py <Warning_Value> <Critcal_Value>
+ Command Line 1:  ./icinga_check_memory.py <Warning_Value> <Critcal_Value> --warning <yes/no> --critical <yes/no> --unknown <yes/no> 
 
  NRPE Examples   ./check_nrpe -H hal2k1.foo.example.com -c icinga_check_memory.py -a 20 10
 
- Local Example:  ./icinga_check_memory.py 20 10
+ Local Example:  ./icinga_check_memory.py 20 10  --critical no --warning yes
 
  TODO:
 
@@ -28,71 +28,104 @@ from datetime import datetime
 import sys
 import subprocess
 import psutil
-
+import argparse
 import CEng_python_lib as ceng_lib
-import settings
-
-# Get the commandline argumentts
-mem_warning = sys.argv[1]
-mem_critical  = sys.argv[2]
 
 # Get the hostname
 machine_name = ceng_lib.get_local_hostname()
 
 def main():
 
-   try:
-       mem_warning, mem_critical
-   except NameError:
-       print('Please enter a value for [mem_warning] and [mem_critical]')
-   else:
-       pass
+ # enable default alerting
+  OK = 0
+  WARNING = 1
+  CRITICAL = 2
+  UNKNOWN = 3
 
-   # Execution start time
-   start_time = datetime.now()
+  parser = argparse.ArgumentParser(description='Retrieve the current Memory Utilization of a particular Host.  Additional Performance Data is outputted along with Standard Status Information.')
+  parser.add_argument('warning_threshold', type=int, help='the percentage of available memory left before entering a warning state')
+  parser.add_argument('critical_threshold', type=int, help='the percentage of memory left before entering a critical state')
+  parser.add_argument('--warning',  choices=['yes', 'no'], default ='yes', help='enable warning alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--critical',  choices=['yes', 'no'], default ='yes', help='enable critical alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--unknown',  choices=['yes', 'no'],  default ='yes', help='enable unknown alerts and status\'s for this check (default: yes)')
+  args = vars(parser.parse_args())
+  
+  # check for a ctitical state, if so and warning is not set to no, then set critical to warning
+  if args['critical']:
+    if args['critical'] == 'no' and args['warning'] == 'no':
+      CRITICAL = OK
+    elif args['critical'] == 'no' and args['warning'] != 'no':
+      CRITICAL = WARNING
+  else:
+    CRITICAL = CRITICAL
+  
+  # check for a warning state, if so and critical is not set to no, then set warning to critical
+  if args['warning']:
+    if args['warning'] == 'no' and args['critical'] == 'no':
+      WARNING = OK
+    elif args['warning'] == 'no' and args['critical'] != 'no':
+      WARNING = OK
+  else:
+    WARNING = WARNING 
+  
+  # if unknown is set to no, then set unknown to ok
+  if args['unknown']:
+    if args['unknown'] == 'no':
+      UNKNOWN = OK
+    else:
+      UNKNOWN = UNKNOWN
 
-   # Get the memory stat
-   memory = psutil.virtual_memory()
+  if args['warning_threshold']:
+    WarningThreshold = args['warning_threshold']
 
-   # The script run time
-   run_time = datetime.now() - start_time
+  if args['critical_threshold']:
+    CriticalThreshold = args['critical_threshold']
+
+  # Execution start time
+  start_time = datetime.now()
+
+  # Get the memory stat
+  memory = psutil.virtual_memory()
+
+  # The script run time
+  run_time = datetime.now() - start_time
 
 
-   mem_total = memory[0] /1048876
-   mem_percent = memory[2]
-   mem_free = memory[4] /1048876
-   mem_active = memory[5] /1048876
-   mem_caches = ( memory[7] + memory[8] ) /1048876
+  mem_total = memory[0] /1048876
+  mem_percent = memory[2]
+  mem_free = memory[4] /1048876
+  mem_active = memory[5] /1048876
+  mem_caches = ( memory[7] + memory[8] ) /1048876
 
-   if memory[2] >= mem_critical:
-       print('Memory Usage Critical - %s (%sMB)Active; | Total=%sMB;;;; Active=%sMB;;;; Free=%sMB;;;; Cached=%sMB;;;; \'Check_Time\'=%s;;;0.000000;60.000000;' % (mem_percent,
+  if memory[2] >= CriticalThreshold:
+    print('Memory Usage Critical - %s (%sMB)Active; | Total=%sMB;;;; Active=%sMB;;;; Free=%sMB;;;; Cached=%sMB;;;; \'Check_Time\'=%s;;;0.000000;60.000000;' % (mem_percent,
                                                                                                                                                                          mem_active,
                                                                                                                                                                          mem_total,
                                                                                                                                                                          mem_active,
                                                                                                                                                                          mem_free,
                                                                                                                                                                          mem_caches,
                                                                                                                                                                          run_time))
-       sys.exit(2)
+    sys.exit(CRITICAL)
 
-   elif memory[2] >= mem_warning:
-       print('Memory Usage Warning - %s (%sMB)Active; | Total=%sMB;;;; Active=%sMB;;;; Free=%sMB;;;; Cached=%sMB;;;; \'Check_Time\'=%s;;;0.000000;60.000000;' % (mem_percent,
+  elif memory[2] >= WarningThreshold:
+    print('Memory Usage Warning - %s (%sMB)Active; | Total=%sMB;;;; Active=%sMB;;;; Free=%sMB;;;; Cached=%sMB;;;; \'Check_Time\'=%s;;;0.000000;60.000000;' % (mem_percent,
                                                                                                                                                                          mem_active,
                                                                                                                                                                          mem_total,
                                                                                                                                                                          mem_active,
                                                                                                                                                                          mem_free,
                                                                                                                                                                          mem_caches,
                                                                                                                                                                          run_time))
-       sys.exit(1)
+    sys.exit(WARNING)
 
-   else:
-       print('Memory Usage OK - %s (%sMB)Active; | Total=%sMB;;;; Active=%sMB;;;; Free=%sMB;;;; Cached=%sMB;;;; \'Check_Time\'=%s;;;0.000000;60.000000;' % (mem_percent,
+  else:
+    print('Memory Usage OK - %s (%sMB)Active; | Total=%sMB;;;; Active=%sMB;;;; Free=%sMB;;;; Cached=%sMB;;;; \'Check_Time\'=%s;;;0.000000;60.000000;' % (mem_percent,
                                                                                                                                                                          mem_active,
                                                                                                                                                                          mem_total,
                                                                                                                                                                          mem_active,
                                                                                                                                                                          mem_free,
                                                                                                                                                                          mem_caches,
                                                                                                                                                                          run_time))
-       sys.exit(0)
+    sys.exit(OK)
 
 if __name__ == "__main__":
     main()
