a86c4055cb43ed7ffcb95dfa4766257d2de5a025
matt <matt.jones@monster.com>
2014-03-28 16:31:16 -0400

    Convert the check to use argparse
    For details on why this was done please see the README section on features.

--- ServiceChecks/CEng_check_url.py
+++ ServiceChecks/CEng_check_url.py
@@ -14,9 +14,9 @@
 
  Usage:
 
- Command Line 1:  ./CEng_check_url.py <hostname> <url>
+ Command Line 1:  ./CEng_check_url.py <hostname> <url> --warning <yes/no> --critical <yes/no> --unknown <yes/no>
 
- Local Example:  ./CEng_check_url.py <hostname> <url>
+ Local Example:  ./CEng_check_url.py <hostname> <url> --warning no  --unknown no
 
  TODO:
 
@@ -26,62 +26,96 @@ from datetime import datetime
 import socket
 import sys
 import re
-a = re.compile("[0-9]")
-
+import argparse
 import CEng_python_lib as ceng_lib
 
-# Check to see if commandline arguements were entered and if so
-# attempt to validate them with regex's
-if len(sys.argv)  != 4:
-    print('Please enter a server, port,  and a url to check')
-    sys.exit(2)
-else:
-    loc = sys.argv[3]
-    if a.match(sys.argv[2]):
-        port = int(sys.argv[2])
-    else:
-        print('Please enter a valid port number')
-        sys.exit(2)
-    server = ceng_lib.validate_hostname(sys.argv[1])
-    if server:
-        server = sys.argv[1]
-    else:
-        print('Please enter a valid server to connect to')
-        sys.exit(2)
 
 def main():
 
-   # Execution start time
-   start_time = datetime.now()
+  OK = ceng_lib.OK
+  WARNING = ceng_lib.WARNING
+  CRITICAL = ceng_lib.CRITICAL
+  UNKNOWN = ceng_lib.UNKNOWN
 
-   # Open a socket to port a port
-   conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  parser = argparse.ArgumentParser(description='Ppens a socket to a remote port and receives a stream of data.  Follow the examples in CEng_python_lib for examples on verifying output.')
+  parser.add_argument('server', help='the hostname of the machine you wish to connect to')
+  parser.add_argument('port', type=int, help='the port number you wish to connect to')
+  parser.add_argument('url', help='the url you wish to check')
+  parser.add_argument('--warning',  choices=['yes', 'no'], default ='yes', help='enable warning alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--critical',  choices=['yes', 'no'], default ='yes', help='enable critical alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--unknown',  choices=['yes', 'no'],  default ='yes', help='enable unknown alerts and status\'s for this check (default: yes)')
+  args = vars(parser.parse_args())
+  
+  # check for a ctitical state, if so and warning is not set to no, then set critical to warning
+  if args['critical']:
+    if args['critical'] == 'no' and args['warning'] == 'no':
+      CRITICAL = OK
+    elif args['critical'] == 'no' and args['warning'] != 'no':
+      CRITICAL = WARNING
+  else:
+    CRITICAL = CRITICAL
+  
+  # check for a warning state, if so and critical is not set to no, then set warning to critical
+  if args['warning']:
+    if args['warning'] == 'no' and args['critical'] == 'no':
+      WARNING = OK
+    elif args['warning'] == 'no' and args['critical'] != 'no':
+      WARNING = OK
+  else:
+    WARNING = WARNING 
+  
+  # if unknown is set to no, then set unknown to ok
+  if args['unknown']:
+    if args['unknown'] == 'no':
+      UNKNOWN = OK
+    else:
+      UNKNOWN = UNKNOWN
 
-   try:
-       conn.connect((server, port))
-   except IOError:
-       print('There is no service listening on port 8080. Make sure the service is running and listening on port 8080')
-       sys.exit(2)
+  if args['port']:
+    Port = args['port'] 
+
+  if args['server']:
+    Server = ceng_lib.validate_hostname(args['server'])
+    if Server:
+      Server = args['server']
+    else:
+      print "Please enter a valid hostname to connect to"
+      sys.exit('CRITICAL')
+
+  if args['url']:
+    Url = args['url'] 
+
+  
+  # Execution start time
+  start_time = datetime.now()
+
+
+  # Open a socket to port a port
+  conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  try:
+    conn.connect((Server, Port))
+  except IOError:
+    print('There is no service listening on port 8080. Make sure the service is running and listening on port 8080')
+    sys.exit(CRITICAL)
    else:
-       pass
+     pass
 
 
-   # Check for the desired output
-   if loc == 'root':
-       output = ceng_lib.get_port_output(conn)
-       conn.close()
+  # Check for the desired output
+  if Url == 'root':
+    output = ceng_lib.get_port_output(conn)
+    conn.close()
+  elif Url == 'icinga-web':
+    output = ceng_lib.get_icinga_http_output(conn)
+    run_time = datetime.now() - start_time
 
-   elif loc == 'icinga-web':
-       output = ceng_lib.get_icinga_http_output(conn)
-       run_time = datetime.now() - start_time
-
-   if output:
-       print('%s; | \'Check_Time\'=%s;;;0.000000;60.000000;' % (output, run_time))
-       sys.exit(0)
-   else:
+  if output:
+    print('%s; | \'Check_Time\'=%s;;;0.000000;60.000000;' % (output, run_time))
+    sys.exit(OK)
+  else:
        print('There is a problem with the service on the server; | \'Check_Time\'=%s;;;0.000000;60.000000;' % (run_time))
-       sys.exit(2)
-
+       sys.exit(CRITICAL)
 
 if __name__ == "__main__":
     main()
