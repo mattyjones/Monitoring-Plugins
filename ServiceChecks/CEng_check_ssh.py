1ff886a4c225a3a1238f6b65c95cfdc8985c3ced
matt <matt.jones@monster.com>
2014-03-28 12:54:21 -0400

    Convert the sript to use argparse
    See the README festures section for deatils on why was done

--- ServiceChecks/CEng_check_ssh.py
+++ ServiceChecks/CEng_check_ssh.py
@@ -13,9 +13,9 @@
 
  Usage:
  
- Command Line 1:  ./CEng_check_ssh.py <hostname> 
+ Command Line 1:  ./CEng_check_ssh.py <hostname> --warning <yes/no> --critical <yes/no> --unknown <yes/no>
 
- Local Example:  ./CEng_check_ssh.py <hostname>
+ Local Example:  ./CEng_check_ssh.py <hostname> --warning no --unknown no
  
  TODO:
 
@@ -24,52 +24,88 @@
 from datetime import datetime
 import socket
 import sys
-
+import argparse
 import CEng_python_lib as ceng_lib
-import settings as st
-
-# Check to see if a commandline arguement was entered and if so
-# attempt to validate it with a regex
-if len(sys.argv)  != 2:
-    print('Please enter a server to connect to')
-    sys.exit(2)
-else:
-    server = ceng_lib.validate_hostname(sys.argv[1])
-    if server:
-        server = sys.argv[1]
-    else:
-        print('Please enter a valid server to connect to')
-        sys.exit(2)
 
 def main():
 
-   # Execution start time
-   start_time = datetime.now()
+ # enable default alerting
+  OK = 0
+  WARNING = 1
+  CRITICAL = 2
+  UNKNOWN = 3
 
-   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   try:
-       result = s.connect_ex((server, 22))
-   except IOError:
-       print('There is no service listening on port 22. Make sure sshd is running and listening on port 22')
-       sys.exit(2)
+  parser = argparse.ArgumentParser(description='Open a socket to a remote SSH port and receives the current SSH
+         server version and protocol.')
+  parser.add_argument('server', help='the server you wish to connect to')
+  parser.add_argument('--warning', choices=['yes', 'no'], default ='yes', help='enable warning alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--critical', choices=['yes', 'no'], default ='yes', help='enable critical alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--unknown', choices=['yes', 'no'], default ='yes', help='enable unknown alerts and status\'s for this check (default: yes)')
+  args = vars(parser.parse_args())
+  
+  # check for a ctitical state, if so and warning is not set to no, then set critical to warning
+  if args['critical']:
+    if args['critical'] == 'no' and args['warning'] == 'no':
+      CRITICAL = OK
+    elif args['critical'] == 'no' and args['warning'] != 'no':
+      CRITICAL = WARNING
+  else:
+    CRITICAL = CRITICAL
+  
+  # check for a warning state, if so and critical is not set to no, then set warning to critical
+  if args['warning']:
+    if args['warning'] == 'no' and args['critical'] == 'no':
+      WARNING = OK
+    elif args['warning'] == 'no' and args['critical'] != 'no':
+      WARNING = OK
+  else:
+    WARNING = WARNING 
+  
+  # if unknown is set to no, then set unknown to ok
+  if args['unknown']:
+    if args['unknown'] == 'no':
+      UNKNOWN = OK
+    else:
+      UNKNOWN = UNKNOWN
+
+  if args['server']:
+    server = ceng_lib.validate_hostname(args['server'])
+    if server:
+      server = args['server']
+  else:
+    print "Please enter a valid hostname to connect to"
+    sys.exit('CRITICAL')
+
+
+  # Execution start time
+  start_time = datetime.now()
+
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  try:
+    result = s.connect_ex((server, 22))
+  except IOError:
+    print('There is no service listening on port 22. Make sure sshd is running and listening on port 22')
+    sys.exit(CRITICAL)
    else:
-       pass
+     pass
 
-   run_time = datetime.now() - start_time
+  run_time = datetime.now() - start_time
 
-   if (result == 0):
-       output = s.recv(256)
-       if not output:
-           print('No output was received from SSH, verify that it is running; | \'Check_Time\'=%s;;;0.000000;60.000000;' % ( run_time))
-       output = output.rsplit()
-       print('%s; | \'Check_Time\'=%s;;;0.000000;60.000000;' % (output[0], run_time))
-       s.close()
-       sys.exit(0)
+  if (result == 0):
+    output = s.recv(256)
+      if not output:
+        print('No output was received from SSH, verify that it is running; | \'Check_Time\'=%s;;;0.000000;60.000000;' % ( run_time))
+        s.close()
+        sys.exit(CRITICAL)
+      output = output.rsplit()
+      print('%s; | \'Check_Time\'=%s;;;0.000000;60.000000;' % (output[0], run_time))
+      s.close()
+      sys.exit(OK)
 
-   else:
-       print('%s; | \'Check_Time\'=%s;;;0.000000;60.000000;' % (st.NO_SOCKET_DATA, run_time))
-       s.close()
-       sys.exit(2)
+  else:
+    print('%s; | \'Check_Time\'=%s;;;0.000000;60.000000;' % ('No data was received from the socket', run_time))
+    s.close()
+    sys.exit(CRITICAL)
 
 if __name__ == "__main__":
     main()   
--- ServiceChecks/CEng_check_swap.py
+++ ServiceChecks/CEng_check_swap.py
@@ -14,11 +14,12 @@
 
  Usage:
 
- Command Line 1:  ./CEng_check_swap.py <Warning_Value> <Critcal_Value>
-
+ Command Line 1:  ./CEng_check_swap.py <Warning_Value> <Critcal_Value> --warning <yes/no> --critical <yes/no> --unknown <yes/no>
+ 
  NRPE Examples   ./check_nrpe -H hal2k1.foo.example.com -c check_swap.py -a 20 10
 
- Local Example:  ./CEng_check_swap.py 20 10
+ Local Example:  ./CEng_check_swap.py 20 10 --critical no --warning yes
+
 
  TODO:
 
@@ -27,82 +28,108 @@
 from datetime import datetime
 import sys
 import psutil
-
+import argparse
 import CEng_python_lib as ceng_lib
 
-if len(sys.argv) != 3:
-    print('Please enter a warning and critical value')
-    sys.exit(2)
+#if len(sys.argv) != 3:
+#    print('Please enter a warning and critical value')
+#    sys.exit(2)
 
 # Get the hostname
 machine_name = ceng_lib.get_local_hostname()
 
 
 def main():
-    swap_warning = sys.argv[1]
-    swap_critical = sys.argv[2]
+  
+  OK = 0
+  WARNING = 1
+  CRITICAL = 2
+  UNKNOWN = 3
 
-    try:
-        swap_warning, swap_critical
-    except NameError:
-        print('Please enter a value for [swap_warning] and [swap_critical]')
+  parser = argparse.ArgumentParser(description='Retrieve the current Memory Utilization of a particular Host.  Additional Performance Data is outputted along with Standard Status Information.')
+  parser.add_argument('warning_threshold', type=int, help='the percentage of available memory left before entering a warning state')
+  parser.add_argument('critical_threshold', type=int, help='the percentage of memory left before entering a critical state')
+  parser.add_argument('--warning',  choices=['yes', 'no'], default ='yes', help='enable warning alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--critical',  choices=['yes', 'no'], default ='yes', help='enable critical alerts and dashboard status\'s for this check (default: yes)')
+  parser.add_argument('--unknown',  choices=['yes', 'no'],  default ='yes', help='enable unknown alerts and status\'s for this check (default: yes)')
+  args = vars(parser.parse_args())
+  
+  # check for a ctitical state, if so and warning is not set to no, then set critical to warning
+  if args['critical']:
+    if args['critical'] == 'no' and args['warning'] == 'no':
+      CRITICAL = OK
+    elif args['critical'] == 'no' and args['warning'] != 'no':
+      CRITICAL = WARNING
+  else:
+    CRITICAL = CRITICAL
+  
+  # check for a warning state, if so and critical is not set to no, then set warning to critical
+  if args['warning']:
+    if args['warning'] == 'no' and args['critical'] == 'no':
+      WARNING = OK
+    elif args['warning'] == 'no' and args['critical'] != 'no':
+      WARNING = OK
+  else:
+    WARNING = WARNING 
+  
+  # if unknown is set to no, then set unknown to ok
+  if args['unknown']:
+    if args['unknown'] == 'no':
+      UNKNOWN = OK
     else:
-        pass
+      UNKNOWN = UNKNOWN
 
-    # Execution start time
-    start_time = datetime.now()
+  if args['warning_threshold']:
+    WarningThreshold = args['warning_threshold']
 
-    # Get the memory stat
-    memory = psutil.swap_memory()
+  if args['critical_threshold']:
+    CriticalThreshold = args['critical_threshold']
 
-    # The script run time
-    run_time = datetime.now() - start_time
+  # Execution start time
+  start_time = datetime.now()
 
-    swap_total = memory[0] / 1048876
-    swap_percent = memory[3]
-    swap_used = memory[1] / 1048876
+  # Get the memory stat
+  memory = psutil.swap_memory()
 
-    if memory[2] >= swap_critical:
-        print(
-        'Memory Usage Warning - %s (%sMB out of %sMB); | Swap=%sMB;0;0;0;%sMB; \'Check_Time\'=%s;;;0.000000;60.000000;' % (
-        swap_percent,
-        swap_used,
-        swap_total,
-        swap_total,
-        swap_total,
-        run_time))
-        sys.exit(2)
+  # The script run time
+  run_time = datetime.now() - start_time
 
-    elif memory[2] >= swap_warning:
-        print(
-        'Memory Usage Warning - %s (%sMB out of %sMB); | Swap=%sMB;0;0;0;%sMB; \'Check_Time\'=%s;;;0.000000;60.000000;' % (
-        swap_percent,
-        swap_used,
-        swap_total,
-        swap_total,
-        swap_total,
-        run_time))
-        sys.exit(1)
+  swap_total = memory[0] / 1048876
+  swap_percent = memory[3]
+  swap_used = memory[1] / 1048876
 
-    else:
-        print(
-        'Memory Usage Warning - %s (%sMB out of %sMB); | Swap=%sMB;0;0;0;%sMB; \'Check_Time\'=%s;;;0.000000;60.000000;' % (
-        swap_percent,
-        swap_used,
-        swap_total,
-        swap_total,
-        swap_total,
-        run_time))
-        sys.exit(0)
+  if memory[2] >= CriticalThreshold:
+    print(
+      'Memory Usage Warning - %s (%sMB out of %sMB); | Swap=%sMB;0;0;0;%sMB; \'Check_Time\'=%s;;;0.000000;60.000000;' % (
+      swap_percent,
+      swap_used,
+      swap_total,
+      swap_total,
+      swap_total,
+      run_time))
+    sys.exit(CRITICAL)
 
+  elif memory[2] >= WarningThreshold:
+    print(
+      'Memory Usage Warning - %s (%sMB out of %sMB); | Swap=%sMB;0;0;0;%sMB; \'Check_Time\'=%s;;;0.000000;60.000000;' % (
+      swap_percent,
+      swap_used,
+      swap_total,
+      swap_total,
+      swap_total,
+      run_time))
+    sys.exit(WARNING)
+
+  else:
+    print(
+      'Memory Usage Warning - %s (%sMB out of %sMB); | Swap=%sMB;0;0;0;%sMB; \'Check_Time\'=%s;;;0.000000;60.000000;' % (
+      swap_percent,
+      swap_used,
+      swap_total,
+      swap_total,
+      swap_total,
+      run_time))
+    sys.exit(OK)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
